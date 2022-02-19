ee2fa449bd86226cb73e782faeb7fdbd8dafa209
Siddhanta <siddhanta.c@gmail.com>
2022-02-19 18:38:25 +0800

    WIP

--- extract.go
+++ extract.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
 	"io/ioutil"
 	"os"
+	"regexp"
+	"strings"
 
 	"github.com/ledongthuc/pdf"
 	//"github.com/dcu/pdf"
@@ -13,7 +17,10 @@ func extract() {
 	if err != nil {
 		panic(err)
 	}
-	ioutil.WriteFile("rows.txt", []byte(content), 0777)
+	ioutil.WriteFile("unparsed.txt", []byte(content), 0777)
+
+	output := parsePDF(content)
+	ioutil.WriteFile("output.txt", []byte(output), 0777)
 	return
 }
 
@@ -47,4 +54,32 @@ func readPdf(path string) (string, error) {
 		}
 	}
 	return output, nil
+}
+
+const searchPattern = `^(?P<day>\d\d)(?P<month>JAN|FEB|MAR|APR|MAY|JUN|JUL|AUG|SEP|OCT|NOV|DEC)(?P<name>.*)(SingaporeSG|SINGAPORESG)(?P<amount>\d+\.\d+)$`
+
+func parsePDF(content string) string {
+	// mc := regexp.MustCompile(`^(?P<day>[0123][0-9])(?P<month>[A-Z][A-Z][A-Z])(?P<name>.*)(?P<amount>[0-9\.]+)$`)
+	mc := regexp.MustCompile(searchPattern)
+
+	r := strings.NewReader(content)
+	scanner := bufio.NewScanner(r)
+	var output string
+
+	// allTransactions := make([]*transaction, 0)
+	for scanner.Scan() {
+		match := mc.FindStringSubmatch(scanner.Text())
+
+		if len(match) == 0 {
+			continue
+		}
+
+		// allTransactions = append(allTransactions, NewTransaction(match[3], match[4], match[1], match[2]))
+		// if err := scanner.Err(); err != nil {
+		// 	fmt.Println(err)
+		// }
+		output = fmt.Sprintf("%s%s\n", output, match[1:])
+	}
+
+	return output
 }
